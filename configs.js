e4a6da9243940b1568bfe8012a323ab9e957bfb5
smthubakgale <97933623+smthubakgale@users.noreply.github.com>
2024-12-06 08:15:33 +0200

    Update configs.js

--- configs.js
+++ configs.js
@@ -330,9 +330,9 @@
 				 description : "this will be used to get the day , month and time" ,
 				 setter: (subform , inputElement) => {
 					 // Sibling Elements 
-					 var _start = subform.querySelector("#start");
- 					 var _month = subform.querySelector("#month");
-					 var _year = subform.querySelector("#year");
+					 var _start = subform.querySelector("[name='spec_start']");
+ 					 var _month = subform.querySelector("[name='spec_month']");
+					 var _year = subform.querySelector("[name='spec_year']");
 
 					 // Set its value to now if it's null
 					if (inputElement.value === "") {
