7b4e104992265c21a5a3d019ba9824119c26f526
smthubakgale <97933623+smthubakgale@users.noreply.github.com>
2024-12-08 08:53:06 +0200

    Update configs.js

--- configs.js
+++ configs.js
@@ -1433,8 +1433,8 @@ const create =
 			   setter: (subform , inputElement) => { 
 				 // Sibling Elements 
 				 console.log(subform.innerHTML);
-				 console.log(subform.querySelector("[name='spec_users_inherits']"));
-				 var parentElement = subform.querySelector("[name='spec_users_inherits']"); 
+				 console.log(subform.querySelector("[name='spec_users_inherits_empty']"));
+				 var parentElement = subform.querySelector("[name='spec_users_inherits_empty']"); 
 				 const subformSiblings = Array.from(subform.parentNode.children).filter(sibling => sibling.classList.contains('subform') && sibling !== subform);
 
 				 console.log("current siblings " + subformSiblings.length); 
