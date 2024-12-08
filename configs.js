b60b8a462f077f6d321d7a4e41f1a47b264af04c
smthubakgale <97933623+smthubakgale@users.noreply.github.com>
2024-12-08 10:10:15 +0200

    Update configs.js

--- configs.js
+++ configs.js
@@ -1434,18 +1434,15 @@ const create =
 				     linkSibling(sibling , k); 
 				     k++;
 				 });
- 
-				 subform.setAttribute(id , 'true');
+  
 				 const observer = new MutationObserver(() => {
-				  const newSubform = subform.parentNode.querySelector('.subform');
-				  if (!newSubform.hasAttribute(id) && !newSubform.hasAttribute(id + "_linked"))
-				  { 
-				     newSubform.setAttribute(id + "_linked" ,'true');
-				     const subformSiblings2 = Array.from(subform.parentNode.children).filter(sibling => sibling.classList.contains('subform') && sibling !== subform);
-
-				     console.log("item " + subformSiblings.length + " appends child " + (subformSiblings2.length + 1));
-				     linkSibling(newSubform , subformSiblings2.length + 1);
-				  }
+				    const newSubforms = Array.from(subform.parentNode.children).filter(s => s.classList.contains('subform') && s.hasAttribute(id)) && s !== subform);
+				    newSubforms.forEach(newSubform => 
+				    {  
+				         newSubform.setAttribute(id  ,'true'); 
+				         console.log("item " + subformSiblings.length + " appends child " + (newSubforms.length + 1));
+				         linkSibling(newSubform , newSubforms.length + 1); 
+				    }); 
 				});
 				
 				observer.observe(subform.parentNode, {
