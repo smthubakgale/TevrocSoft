7ad1c5649461a446395cb03a29f093fc9fae23ef
smthubakgale <97933623+smthubakgale@users.noreply.github.com>
2024-12-13 09:48:53 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,10 +4,7 @@ import {
   } from './configs.js';
 
 //------------------------------------: Shop 
- // Initialize EmailJS 
-(function() {
-    emailjs.init('OiMEAcHFuztUoVBi0');
-  })();
+ // Initialize EmailJS  
 
 function sendEmail(_name , _message , _email , formStatus , form , _subject = "Missing Subject")
 {
@@ -33,64 +30,27 @@ function sendEmail(_name , _message , _email , formStatus , form , _subject = "M
 	}
 	*/
 
-const queryString = new URLSearchParams(params).toString();
-
+	const queryString = new URLSearchParams(params).toString();
+	
 	console.log(queryString);
-
-fetch(`https://tevrocsoftapi.netlify.app/.netlify/functions/api/send-email2?${queryString}`, {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json'
-  }
-})
-.then(response => response.text())
-.then(data => console.log(data))
-.catch((error) => {
-  console.log('FAILED...', error);
-});
-
-	return;
 	
-   const brevvo_Key = "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"; 
-   fetch('https://api.brevo.com/v3/smtp/email', {
-  method: 'POST',
-  headers: {
-    'accept': 'application/json',
-    'api-key': brevvo_Key ,
-    'content-type': 'application/json'
-  },
-  body: JSON.stringify({
-    "sender": {
-      "name": name ,
-      "email": email
-    },
-    "to": [
-      {
-        "email": "smthubakgale@gmail.com",
-        "name": "Mabalane Thubakgale"
-      }
-    ],
-    "subject": subject,
-    "htmlContent": `<html><head></head><body${message}</body></html>`
-  })
-})
-.then(response => response.json())
-.then(data => console.log(data))
-.catch(error => console.error(error));
-	
-    emailjs.send("service_44zo6pj","template_m3vjj5x",{
-    from_name: name ,
-    html_message: `<html><head></head><body>${message}</body></html>` ,
-    from_email: email ,
-    reply_to: email,
-    }).then(() => {
-                console.log('SUCCESS!');
-                formStatus.innerHTML = 'Message sent successfully!';
-                form.reset();
-            }, (error) => {
-                console.log('FAILED...', error);
-                formStatus.innerHTML = 'Error sending message. Please try again.';
-            });
+	fetch(`https://tevrocsoftapi.netlify.app/.netlify/functions/api/send-email2?${queryString}`, {
+	  method: 'POST',
+	  headers: {
+	    'Content-Type': 'application/json'
+	  }
+	})
+	.then(response => response.text())
+	.then((data) => {
+	    console.log(data)
+	    formStatus.innerHTML = 'Message sent successfully!';
+	    form.reset();
+	})
+	.catch((error) => {
+	  console.log('FAILED...', error);
+	  formStatus.innerHTML = 'Error sending message. Please try again.';
+	});
+
 }
 
 var fm1 = document.getElementById("send-email-1").parentNode;
