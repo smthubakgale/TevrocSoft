bdfa92a71952a14b360053084358464d5e15ffa5
smthubakgale <97933623+smthubakgale@users.noreply.github.com>
2024-12-13 07:22:35 +0200

    Update script.js

--- script.js
+++ script.js
@@ -12,19 +12,11 @@ import {
 function sendEmail(name , message , email , formStatus , form , subject = "Missing Subject")
 { 
 	fetch('https://tevrocsoftapi.netlify.app/.netlify/functions/api/send-email2', {
-  method: 'GET',
-  headers: {
-    'Content-Type': 'application/json'
-  },
-  body: JSON.stringify({
-    name: 'Your Name',
-    email: 'your-email@example.com',
-    message: 'Hello, this is a test email!'
-  })
+  mode: 'no-cors'
 })
-.then(response => response.json())
-.then(data => console.log(data))
-.catch(error => console.error(error));
+  .then(response => response.json())
+  .then(data => console.log(data))
+  .catch(error => console.error(error));
 
 	return;
 	
