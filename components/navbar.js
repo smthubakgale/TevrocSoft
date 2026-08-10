ccaa4c06ec0f6a2270a7c84c9ef66b4f879762e8
smthubakgale <97933623+smthubakgale@users.noreply.github.com>
2026-08-10 17:09:22 +0200

    Auto-publish: components/navbar.js - Mon 08/10/2026 17:09:22.58

--- components/navbar.js
+++ components/navbar.js
@@ -366,9 +366,9 @@ class CustomNavbar extends HTMLElement {
 
         .bottom-nav {
           position: fixed;
-          left: 0;
-          right: 0;
+          left: 0; 
           bottom: 0;
+          width:100vw;
           z-index: 1025;
           display: flex;
           justify-content: space-around;
